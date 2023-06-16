43a031a6fff2f32adb6a8bcd6f8135c1af5bfda3
Badusha <113815355+badusha01@users.noreply.github.com>
2023-06-16 16:07:29 +0530

    second commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,11 @@
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import user from "../models/User.js"
-import User from "../models/User.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-// REGISTER USER
- 
+/* REGISTER USER */
 export const register = async (req, res) => {
     try {
-    const {
+      const {
         firstName,
         lastName,
         email,
@@ -15,13 +13,13 @@ export const register = async (req, res) => {
         picturePath,
         friends,
         location,
-        occupation
-    } = req.body 
-
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const newUser = new User({
+        occupation,
+      } = req.body;
+  
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+  
+      const newUser = new User({
         firstName,
         lastName,
         email,
@@ -31,33 +29,30 @@ export const register = async (req, res) => {
         location,
         occupation,
         viewedProfile: Math.floor(Math.random() * 10000),
-        impressions: Math.floor(Math.random() * 10000)
-    });
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-
-    } catch(err) {
+        impressions: Math.floor(Math.random() * 10000),
+      });
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (err) {
       res.status(500).json({ error: err.message });
     }
+  };
 
-}
 
-// LOGGING IN 
+  /* LOGGING IN */
 export const login = async (req, res) => {
     try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email: email });
-        if(!user) return res.status(400).json({msg: "User does not exits"})
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch) return res.status(400).json({ msg: "Invalid credentials "})
-        
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-        delete user.password;
-        res.status(200).json({ token,user })
-    }   catch(err){
-        
-        res.status(500).json({ error: err.message })
+      const { email, password } = req.body;
+      const user = await User.findOne({ email: email });
+      if (!user) return res.status(400).json({ msg: "User does not exist. " });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+  
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      delete user.password;
+      res.status(200).json({ token, user });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
-    
-}
+  };
